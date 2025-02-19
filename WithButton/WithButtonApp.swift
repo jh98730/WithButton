0c377e47aea7b3a8fe9fd84ba277284bd9bcb69e
John Howard <john.k.howard@gmail.com>
2025-02-19 14:29:31 -0500

    Initial Commit

---
+++ WithButton/WithButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  WithButtonApp.swift
+//  WithButton
+//
+//  Created by John Howard on 2/19/25.
+//
+
+import SwiftUI
+
+@main
+struct WithButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
